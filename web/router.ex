5cacf4193bf8d2e4f95c22986dd1f3adc666369e
Florian Kempenich <shockn745@gmail.com>
2017-02-14 08:13:49 +0000

    Add create new user

--- web/router.ex
+++ web/router.ex
@@ -13,12 +13,18 @@ defmodule Rumbl.Router do
     plug :accepts, ["json"]
   end
 
+  # Note: mix phoenix.routes to see all the available routes of the application
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
 
-    get "/users",     UserController, :index
-    get "/users/:id", UserController, :show
-    get "/",          PageController, :index
+    get "/", PageController, :index
+
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+    # Equivalent to:
+    # get  "/users",     UserController, :index
+    # get  "/users/:id", UserController, :show
+    # put  "/users/new", UserController, :new
+    # post "/users",     UserController, :create
   end
 
   # Other scopes may use custom stacks.
